4b93283a84320b4f3d8ce7c21f1adff5fb652cd0
naitou <s.naitou.sys23@morijyobi.ac.jp>
2023-11-11 21:15:34 +0900

    プログラムの仕様変更

--- Main.java
+++ Main.java
@@ -3,13 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+	int i = 0;
+	while(i <= 1){
+		System.out.println("身長を入力(例：1.75)");
+		double n = Double.parseDouble(sc.nextLine());
+		System.out.println("体重を入力(例：65.5)");
+		double x = Double.parseDouble(sc.nextLine());
 
-        System.out.println("身長を入力(例：1.75)");
-        double n = Double.parseDouble(sc.nextLine());
-        System.out.println("体重を入力(例：65.5)");
-        double x = Double.parseDouble(sc.nextLine());
-
-        double j = (x / (n * n));
-        System.out.println((String.format("%.2f", j)) + "%");
+		double j = (x / (n * n));
+		System.out.println((String.format("%.2f", j)));
+		System.out.println("");
+		i++;
+	}
     }
 }
