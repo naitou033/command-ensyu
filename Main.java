0dc5c5ca9ca92ae19bf918ec433b376bb5272d9a
naitou <s.naitou.sys23@morijyobi.ac.jp>
2023-11-11 21:06:09 +0900

    Mainのコミット

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("身長を入力(例：1.75)");
+        double n = Double.parseDouble(sc.nextLine());
+        System.out.println("体重を入力(例：65.5)");
+        double x = Double.parseDouble(sc.nextLine());
+
+        double j = (x / (n * n));
+        System.out.println((String.format("%.2f", j)) + "%");
+    }
+}
